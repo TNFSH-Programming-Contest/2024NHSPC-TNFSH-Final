40878953625f2091fbc48ebfe1200bb903260e55
Same0620 <131766594+Same0620@users.noreply.github.com>
2024-09-24 01:55:03 +0800

    Upd

--- pE/solution/correct.cpp
+++ pE/solution/correct.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define lli long long int
+lli dp[1000005], ndp[1000005];
+void sol(){
+	
+}
 int main() {
-	// TODO
-	int n;
-	cin >> n;
-	cout << 0 << endl;
-	return 0;
+	ios_base::sync_with_stdio(0);cin.tie(0);
+	sol();
 }
