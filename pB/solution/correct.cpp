1cbaedf8f61c985a445d26289fd7c552a394b38b
Samuel <138226245+GuoYuKai-SaMuEl@users.noreply.github.com>
2024-09-24 12:01:39 +0800

    update

--- pB/solution/correct.cpp
+++ pB/solution/correct.cpp
@@ -1,10 +1,15 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-	// TODO
-	int n;
-	cin >> n;
-	cout << 0 << endl;
-	return 0;
+int way[105],t[15],a[105][15],b[105][15],c[105][15],x[105][15][15];
+int main(){
+	int n,m;
+	cin>>n>>m;
+	for(int i=1;i<=n;i++)cin>>way[i];
+	for(int i=1;i<=m;i++)cin>>t[i];
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=way[i];j++){
+			cin>>a[i][j];
+			for(int k=1;k<=a[i][j];k++)cin>>x[i][j][k];
+		}
+	}
 }
