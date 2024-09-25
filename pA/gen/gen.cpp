3776703af1c955ba5c998d001de0c7ec4326e7d8
Same0620 <131766594+Same0620@users.noreply.github.com>
2024-09-26 01:13:34 +0800

    Update gen.cpp

--- pA/gen/gen.cpp
+++ pA/gen/gen.cpp
@@ -4,11 +4,33 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	registerGen(argc, argv, 1);
-	// about testlib, see https://codeforces.com/blog/entry/18291
-
-	// TODO
-	int n = atoi(argv[1]);
-	cout << n << endl;
-
+	int subtask=atoi(argv[1]);int idx = atoi(argv[2]);
+	long long int n, q;
+	if(subtask==1) n=3000, q=3000;
+	else n=5000000, q=5000000;
+	cout << n << ' ' << q << "\n";
+	vector<int> v;
+	for(int i=1;i<=n;i++)v.push_back(i);
+	random_shuffle(v.begin(), v.end());
+	int it = 0;
+	for(int i=0;i<q;i++){
+		int ch;
+		if(it!=n-1) ch = rnd.next(1, 3);
+		else {
+			ch = rnd.next(1, 2);
+			if(ch==2)ch=3;
+		}
+		if(ch==1){
+			int l = rnd.next(1, n);
+			int r = rnd.next(l, n);
+			int k = rnd.next(1, 1000000000);
+			cout << ch << ' ' << l << ' ' << r << ' ' << k << "\n";
+		}else if(ch==2){
+			cout << ch << ' ' << v[it] << "\n";
+			it++;
+		}else{
+			cout << ch << ' ' << rnd.next(1, n) << "\n";
+		}
+	}
 	return 0;
 }
