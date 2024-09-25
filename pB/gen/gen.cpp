89d4d3040e067c320fa52f5a5f520e39a3c1769f
Samuel <138226245+GuoYuKai-SaMuEl@users.noreply.github.com>
2024-09-25 14:32:07 +0800

    update

--- pB/gen/gen.cpp
+++ pB/gen/gen.cpp
@@ -1,14 +1,33 @@
-#include <bits/stdc++.h>
 #include "testlib.h"
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(int argc, char* argv[]) {
+int way[105],t[15],a[105][15],b[105][15],c[105][15],shufa[105],shufb[15];
+inline int randomint(double l,double r){
+	return pow(10.,rnd.next(l,r));
+}
+int main(int argc, char* argv[]){
 	registerGen(argc, argv, 1);
-	// about testlib, see https://codeforces.com/blog/entry/18291
-
-	// TODO
-	int n = atoi(argv[1]);
-	cout << n << endl;
-
-	return 0;
+    int subtask=atoi(argv[1]),idx=atoi(argv[2]);
+	int n=(idx%2?rnd.next(1,99):100),m=10;
+	cout<<n<<' '<<m<<'\n';
+	for(int i=0;i<n;i++)shufa[i]=i+1;
+	for(int i=0;i<m;i++)shufb[i]=i+1;
+	for(int i=0;i<n;i++)way[i]=(subtask==5?1:idx%3==1?10:rnd.next(1,10));
+	for(int i=0;i<n;i++)cout<<way[i]<<" \n"[i==n-1];
+	for(int i=0;i<m;i++)t[i]=randomint(0,9);
+	for(int i=0;i<m;i++)cout<<t[i]<<" \n"[i==m-1];
+	for(int i=0;i<n;i++)for(int j=0;j<way[i];j++){
+		a[i][j]=(subtask==1||subtask==2?0:subtask==3||subtask==4?1:rnd.next(0,min(n,10)));
+		cout<<a[i][j]<<' ';
+		shuffle(shufa,shufa+n);
+		for(int k=0;k<a[i][j];k++)cout<<shufa[k]<<' '<<rnd.next(1,10)<<" \n"[k==a[i][j]-1];
+		if(!a[i][j])cout<<'\n';
+		b[i][j]=(subtask==1||subtask==3?0:rnd.next(0,10));
+		cout<<b[i][j]<<' ';
+		shuffle(shufb,shufb+m);
+		for(int k=0;k<b[i][j];k++)cout<<shufb[k]<<" \n"[k==b[i][j]-1];
+		if(!b[i][j])cout<<'\n';
+		c[i][j]=rnd.next(0,100);
+		cout<<c[i][j]<<'\n';
+	}
 }
--- pB/solution/correct.cpp
+++ pB/solution/correct.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int way[105],t[15],a[105][15],b[105][15],c[105][15],x[105][15][15],y[105][15][15],z[105][15][15],in[105][15];
-long long int dis[105][15];
+long long int dis[105][15],now[105],ans[105];
 vector<pair<int,int>>out[105];
 int main(){
 	int n,m;
@@ -9,20 +9,55 @@ int main(){
 	for(int i=1;i<=n;i++)cin>>way[i];
 	for(int i=1;i<=m;i++)cin>>t[i];
 	for(int i=1;i<=n;i++){
+		ans[i]=1e15;
 		for(int j=1;j<=way[i];j++){
 			cin>>a[i][j];
-			for(int k=1;k<=a[i][j];k++)cin>>x[i][j][k]>>y[i][j][k];
+			for(int k=1;k<=a[i][j];k++){
+				cin>>x[i][j][k]>>y[i][j][k];
+				out[x[i][j][k]].push_back({i,j});
+			}
             cin>>b[i][j];
             for(int k=1;k<=b[i][j];k++)cin>>z[i][j][k];
             cin>>c[i][j];
 		}
 	}
-    for(int p=0;p<(1<<m);p++){
-        for(int i=1;i<=n;i++)for(int j=1;j<=way[i];j++){
-            dis[i][j]=1e15;
-            in[i][j]=a[i][j];
-            for(int k=1;k<=b[i][j];k++)if(~p&(1<<(z[i][j][k]-1)))in[i][j]++;
-
+    for(int per=0;per<(1<<m);per++){
+		cout<<per<<'\n';
+        for(int i=1;i<=n;i++){
+			now[i]=1e15;
+			for(int j=1;j<=way[i];j++){
+            	dis[i][j]=1e15;
+            	in[i][j]=a[i][j];
+            	for(int k=1;k<=b[i][j];k++)if((~per)&(1<<(z[i][j][k]-1)))in[i][j]++;
+				cout<<i<<' '<<j<<' '<<in[i][j]<<'\n';
+			}
         }
+		queue<pair<int,int>>qu;//store the (item_id,way_id) which can be maken
+		set<pair<int,int>>se;//store the (dis,item_id) whose shortest time is determined and can do Dijkstra
+		for(int i=1;i<=n;i++)for(int j=1;j<=way[i];j++)if(!in[i][j])qu.push({i,j});
+		while(se.size()||qu.size()){
+			while(qu.size()){
+				auto [i,j]=qu.front();
+				qu.pop();
+				dis[i][j]=c[i][j];
+				for(int k=1;k<=a[i][j];k++){
+					dis[i][j]+=now[x[i][j][k]]*y[i][j][k];
+				}
+				if(dis[i][j]>=1e15)dis[i][j]=1e15;
+				else se.insert({dis[i][j],i});
+			}
+			if(se.empty())continue;
+			auto [temp,i]=*se.begin();
+			se.erase(se.begin());
+			if(now[i]!=1e15)continue;
+			now[i]=temp;
+			for(auto p:out[i]){
+				if(--in[p.first][p.second]==0)qu.push(p);
+			}
+		}
+		long long int tsum=0;
+		for(int i=1;i<=m;i++)if(per&(1<<(i-1)))tsum+=t[i];
+		for(int i=1;i<=n;i++)ans[i]=min(ans[i],now[i]+tsum);
     }
+	for(int i=1;i<=n;i++)cout<<ans[i]<<'\n';
 }
