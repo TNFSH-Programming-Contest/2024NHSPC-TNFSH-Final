8bdd57e8f66814da7cbea5ac6a1d75e6c044154b
Same0620 <131766594+Same0620@users.noreply.github.com>
2024-09-26 15:48:53 +0800

    Update gen.cpp

--- pA/gen/gen.cpp
+++ pA/gen/gen.cpp
@@ -5,31 +5,147 @@ using namespace std;
 int main(int argc, char* argv[]) {
 	registerGen(argc, argv, 1);
 	int subtask=atoi(argv[1]);
-	long long int n, q;
-	if(subtask==1) n=3000, q=3000;
+	int n, q;
+	if(subtask==1) n=500, q=500;
+	else if(subtask==5) n=1000000, q=1000000;
 	else n=5000000, q=5000000;
 	cout << n << ' ' << q << "\n";
+	
 	vector<int> v;
 	for(int i=1;i<=n;i++)v.push_back(i);
 	shuffle( v.begin(), v.end() );
 	int it = 0;
-	for(int i=0;i<q;i++){
-		int ch;
-		if(it!=n-1) ch = rnd.next(1, 3);
-		else {
+	
+	if(subtask==1||subtask==5){
+		for(int i=0;i<q;i++){
+			int ch;
+			if(it!=n-1) ch = rnd.next(1, 3);
+			else {
+				ch = rnd.next(1, 2);
+				if(ch==2)ch=3;
+			}
+			if(ch==1){
+				if(i%3==0){
+				int l = rnd.next(1, n);
+				int r = l;
+				int k = rnd.next(1, 100000);
+			}else if(i%3==1){
+				int cc = n/10;
+				int l = rnd.next(1, cc);
+				int r = rnd.next(n-cc, n);
+				int k = rnd.next(10000, 100000);
+			}else if(i%3==2){
+				int l = rnd.next(1, n);
+				int r = n;
+				int k = rnd.next(50000, 100000);
+			}
+			}else if(ch==2){
+				cout << ch << ' ' << v[it] << "\n";
+				it++;
+			}else{
+				cout << ch << ' ' << rnd.next(1, n) << "\n";
+			}
+		}
+	}else if(subtask==2){
+		for(int i=0;i<q;i++){
+			if(i%3==0){
+				int l = rnd.next(1, n);
+				int r = l;
+				int k = rnd.next(1, 100000);
+			}else if(i%3==1){
+				int cc = n/10;
+				int l = rnd.next(1, cc);
+				int r = rnd.next(n-cc, n);
+				int k = rnd.next(10000, 100000);
+			}else if(i%3==2){
+				int l = rnd.next(1, n);
+				int r = n;
+				int k = rnd.next(50000, 100000);
+			}
+			cout << 1 << ' ' << l << ' ' << r << ' ' << k << "\n";
+		}
+	}else if(subtask==3){
+		for(int i=0;i<q;i++){
+			int ch;
 			ch = rnd.next(1, 2);
 			if(ch==2)ch=3;
+			if(ch==1){
+				if(i%3==0){
+					int l = rnd.next(1, n);
+					int r = l;
+					int k = rnd.next(1, 100000);
+				}else if(i%3==1){
+					int cc = n/10;
+					int l = rnd.next(1, c);
+					int r = rnd.next(n-c, n);
+					int k = rnd.next(10000, 100000);
+				}else if(i%3==2){
+					int l = rnd.next(1, n);
+					int r = n;
+					int k = rnd.next(50000, 100000);
+				}
+				cout << ch << ' ' << l << ' ' << r << ' ' << k << "\n";
+			}
+			else{
+				cout << ch << ' ' << rnd.next(1, n) << "\n";
+			}
 		}
-		if(ch==1){
-			int l = rnd.next(1LL, n);
-			int r = rnd.next(1LL, n);
-			int k = rnd.next(1, 1000000000);
-			cout << ch << ' ' << l << ' ' << r << ' ' << k << "\n";
-		}else if(ch==2){
-			cout << ch << ' ' << v[it] << "\n";
-			it++;
-		}else{
-			cout << ch << ' ' << rnd.next(1LL, n) << "\n";
+	}else if(subtask==4){
+		for(int i=0;i<q;i++){
+			int ch;
+			ch = rnd.next(1, 2);
+			if(it==n-1)ch=1;
+			if(ch==1){
+				if(i%3==0){
+					int l = rnd.next(1, n);
+					int r = l;
+					int k = rnd.next(1, 100000);
+				}else if(i%3==1){
+					int cc = n/10;
+					int l = rnd.next(1, c);
+					int r = rnd.next(n-c, n);
+					int k = rnd.next(10000, 100000);
+				}else if(i%3==2){
+					int l = rnd.next(1, n);
+					int r = n;
+					int k = rnd.next(50000, 100000);
+				}
+				cout << ch << ' ' << l << ' ' << r << ' ' << k << "\n";
+			}else {
+				cout << ch << ' ' << v[it] << "\n";
+				it++;
+			}
+		}
+	}else if(subtask==6){
+		for(int i=0;i<q;i++){
+			int ch;
+			if(it!=n-1) ch = rnd.next(1, 3);
+			else {
+				ch = rnd.next(1, 2);
+				if(ch==2)ch=3;
+			}
+			if(ch==1){
+				if(i%7<=4){
+					int l = rnd.next(1, n);
+					int r = l;
+					int k = rnd.next(1, 100000);
+				}else if(i%7==5){
+					int cc = n/10;
+					int l = rnd.next(1, c);
+					int r = rnd.next(n-c, n);
+					int k = rnd.next(10000, 100000);
+				}else if(i%7==6){
+					int l = rnd.next(1, n);
+					int r = n;
+					int k = rnd.next(50000, 100000);
+				}
+				cout << ch << ' ' << l << ' ' << r << ' ' << k << "\n";
+			}else if(ch==2){
+				cout << ch << ' ' << v[it] << "\n";
+				it++;
+			}else{
+				cout << ch << ' ' << rnd.next(1, n) << "\n";
+			}
 		}
 	}
 	return 0;
